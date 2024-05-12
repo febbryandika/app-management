5352fe6e898ccd8828e1c5cc04828066492bf8ea
olanseverson <olanseverson13@gmail.com>
2024-05-12 16:06:46 +0700

    feat: main program

--- cli/main.go
+++ cli/main.go
@@ -1,21 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"simulasi-pair-project/config"
+	"os"
+	"simulasi-pair-project/handler"
+	"strings"
 )
 
-func main() {
-	db, err := config.GetDB()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
+func Report() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("=========================")
+		fmt.Println("       Fitur Product")
+		fmt.Println("=========================\n")
 
-	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("1. Tambah Produk ")
+		fmt.Println("2. Ubah Stok Produk")
+		fmt.Println("3. Tambah Staff")
+		fmt.Println("4. Rekap Penjualan")
+		fmt.Println("5. Exit\n")
+		fmt.Print("Silahkan masukkan nomor: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			handler.TambahProduk()
+		case "2":
+			handler.UbahStok()
+		case "3":
+			handler.TambahStaff()
+		case "4":
+			handler.RekapPenjualan()
+		case "5":
+			fmt.Println("Exit fitur....")
+			return
+		default:
+			fmt.Println("Input invalid!!!")
+		}
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("=========================")
+		fmt.Println("Selamat datang di Product")
+		fmt.Println("=========================\n")
+
+		fmt.Println("1. Login ")
+		fmt.Println("2. Register")
+		fmt.Println("3. Forgot Password")
+		fmt.Println("4. Exit\n")
+		fmt.Print("Silahkan masukkan nomor: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			user, err := handler.Login()
+			if err != nil {
+				fmt.Println("Error connecting to db", err)
+				break
+			}
+			_ = user
+			Report()
+		case "2":
+			handler.Register()
+		case "3":
+			handler.ForgotPassword()
+		case "4":
+			fmt.Println("Exiting program...")
+			return
+		default:
+			fmt.Println("Input invalid:")
+		}
 	}
 }
--- config/database.go
+++ config/database.go
@@ -3,9 +3,10 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	"os"
 )
 
 func GetDB() (*sql.DB, error) {
@@ -19,5 +20,17 @@ func GetDB() (*sql.DB, error) {
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME")))
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
 	return db, err
 }
