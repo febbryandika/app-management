f790ec4da6de9db8ac65daa91b15ee066ca62dda
febbryandika <febbryandika97@gmail.com>
2024-05-12 15:08:23 +0700

    initial commit

---
+++ cli/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"simulasi-pair-project/config"
+)
+
+func main() {
+	db, err := config.GetDB()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
---
+++ config/database.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+func GetDB() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME")))
+	return db, err
+}
